5b449c1580c1ffc45d4b0d7dd1283fcda8289d59
jessievandam <jessievandam@hotmail.com>
2022-02-23 12:04:36 +0100

    adjusted identification file

--- function_files/collision_identification.m
+++ function_files/collision_identification.m
@@ -6,8 +6,7 @@ function [magEstForceFinal, magEstForceLPFfinal, offset] = collision_identificat
     % params
     K = size(magEstForce,1);
     H = size(magEstForce,2);
-    T_fixed = 0.2;
-    
+
     %% apply LPF to estimated force: offset
     
     fc = 0.5; % cut-off frequency [Hz]
@@ -28,19 +27,8 @@ function [magEstForceFinal, magEstForceLPFfinal, offset] = collision_identificat
             for i = 2:endInd
                 if collision{k,h}(i) == 0
                     % if collision just ended 
-                    if collision{k,h}(i-1) == 1 
-                        offset{k,h}(i) = offsetFixed;
-                        % start counting
-                        count = 0; 
-                    elseif count < ceil(T_fixed/Ts)
-                       % keep offset fixed for T_fixed sec
-                       offset{k,h}(i) = offsetFixed;
-                       count = count + 1;
-                    else
-                       offset{k,h}(i) = alpha*offset{k,h}(i-1) + (1-alpha)*magEstForce{k,h}(i);
-                    end
                     runOnce = 1;
-%                     offset{k,h}(i) = alpha*offset{k,h}(i-1) + (1-alpha)*magEstForce{k,h}(i);
+                    offset{k,h}(i) = alpha*offset{k,h}(i-1) + (1-alpha)*magEstForce{k,h}(i);
                 else
                     % freeze offset
                     if runOnce == 1 
